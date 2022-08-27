c95ce8836955c047e7c91fd1fe54f6a6d4ef9aeb
Aimal Mansory <aimalmansory21@gmail.com>
2022-08-27 15:41:19 -0700

    added git ignore file

--- src/test/java/class01/gitBasics.java
+++ src/test/java/class01/gitBasics.java
@@ -5,5 +5,6 @@ public class gitBasics {
         // Write down a code that checks temperature form a website and then
         // prints out that is a beautiful day or not
         System.out.println("Today us a beautiful day ");
+        System.out.println("Today is not a beautiful night");
     }
 }
